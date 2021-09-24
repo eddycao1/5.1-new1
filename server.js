b1fa2f101e63274a0641e8aada8b8611c181c320
eddycao1 <81790566+eddycao1@users.noreply.github.com>
2021-09-24 16:53:22 +1000

    1

--- server.js
+++ server.js
@@ -566,7 +566,7 @@ app.use(passport.session());
 app.set('view engine','ejs');
 
 app.get('/success',function(req,res){
-    res.redirect("https://morning-forest-71134.herokuapp.com/index.html")
+    res.redirect(__dirname + "/index.html")
 });
 
 app.get('/error',function(req,res){
@@ -628,7 +628,7 @@ app.post('/charge', (req, res) => {
             currency: 'usd',
             customer: customer.id,
             description: 'Thank you for your generous donation.'
-        })).then(() => res.render('complete.html'))
+        })).then(() => res.redirect('https://morning-forest-71134.herokuapp.com/complete.html'))
             .catch(err => console.log(err))
     } catch (err) { res.send(err) }
 })
